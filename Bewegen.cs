b906d6f626d6cb968a6cfb7f8d1632182d3080a6
jeni1998 <45996236+jeni1998@users.noreply.github.com>
2018-12-19 12:53:48 +0100

    Add files via upload

---
+++ Bewegen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bewegen : MonoBehaviour
+{
+
+    // Use this for initialization
+    [SerializeField] float MoveSpeed = 0.0f;
+    float Pos;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Pos = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(Pos * MoveSpeed, 0f);
+        Debug.Log("PENIS");
+    }
+}
